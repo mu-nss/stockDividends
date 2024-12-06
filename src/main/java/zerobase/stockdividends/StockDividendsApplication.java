0f48c28970e255fc73f35c1c453ab5cb86ac467f
mu-nss <msj20161282@gmail.com>
2024-12-06 15:29:36 +0900

    create project

---
+++ src/main/java/zerobase/stockdividends/StockDividendsApplication.java
@@ -0,0 +1,13 @@
+package zerobase.stockdividends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDividendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockDividendsApplication.class, args);
+    }
+
+}
