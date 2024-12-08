74cbf646f8477627d8f11c3faff48cf24c7f4c60
mu-nss <msj20161282@gmail.com>
2024-12-08 13:47:58 +0900

    서비스 설계 및 구현

--- src/main/java/zerobase/stockdividends/StockDividendsApplication.java
+++ src/main/java/zerobase/stockdividends/StockDividendsApplication.java
@@ -9,5 +9,4 @@ public class StockDividendsApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockDividendsApplication.class, args);
     }
-
 }
---
+++ src/main/java/zerobase/stockdividends/web/AuthController.java
@@ -0,0 +1,4 @@
+package zerobase.stockdividends.web;
+
+public class AuthController {
+}
